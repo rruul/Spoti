2748b7980c5a6dc1d96a2ca4f12d8b403fbf2c23
Raul Ortega Zavala <ortega,zavala.raul@gmail.com>
2023-03-08 15:27:41 -0600

    se introdujo la funcion de busqueda

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const cardTop = document.querySelector('#cardTop').content
 const fragment = document.createDocumentFragment()
 const contenido = document.querySelector('#contenido')
+const btnBuscar = document.getElementById('buscador')
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,6 +31,17 @@ const creaCards = () => {
     topTwoHundred.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            //console.log(index, size)
+            if ( index === size -1 ){
+                artists += item.name
+            } else {
+                artists += item.name +'/'
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
 
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
@@ -37,3 +49,21 @@ const creaCards = () => {
     })
     contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () => {
+    console.log('tecla', btnBuscar.value)
+    let otro = btnBuscar.value
+    let expre = new RegExp(`${otro}.*`)
+    let busq = topTwoHundred.filter(song=>expre.test(song.trackMetadata.trackName))
+    if(btnBuscar.value != null){
+    document.getElementById('contenido').innerHTML = ''
+    busq.forEach((bus) => {
+    cardTop.querySelector('.songname').textContent = bus.trackMetadata.trackName
+    const clone = cardTop.cloneNode(true)
+    fragment.appendChild(clone)
+    })
+    contenido.appendChild(fragment)
+    }else{
+    return
+    }
+    })
